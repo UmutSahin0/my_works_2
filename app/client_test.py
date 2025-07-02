cafac0973a4950277b1d24b277379acfb36434d4
UmutSahin0 <umutsahinn1@hotmail.com>
2025-07-02 15:07:07 +0300

    fı

--- app/client_test.py
+++ app/client_test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://51.21.220.24:8000/count"
+url = "http://16.170.253.158:8000/count"
 data = {"number": 10}  # Göndermek istediğin sayı
 
 response = requests.post(url, json=data)
